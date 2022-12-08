ef274c3d648bda400e594f4a33c619f8978a6a63
Cleber Gomes <cleberc@gmail.com>
2022-12-08 20:27:52 -0300

    ENG-10087: Remove rules for scaling instances on azurerm_monitor_autoscale_setting.
    We currently don't have these rules for EC2 sidecar due to the risk of scaling down and closing user connections. The scale set should have a fixed number of instances (minimum, maximum and desired) as we have in AWS EC2. Please see the parameters asg_* in the AWS EC2 module.

--- scaleset_resources.tf
+++ scaleset_resources.tf
@@ -201,6 +201,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "cyral-sidecar-asg" {
 }
 
 resource "azurerm_monitor_autoscale_setting" "monitor-autoscale-setting" {
+  count = var.auto_scale_count
   name                = "${local.name_prefix}-monitor-autoscale-setting"
   resource_group_name = azurerm_resource_group.cyral_sidecar.name
   location            = azurerm_resource_group.cyral_sidecar.location
@@ -210,55 +211,9 @@ resource "azurerm_monitor_autoscale_setting" "monitor-autoscale-setting" {
     name = "defaultProfile"
 
     capacity {
-      default = 1
-      minimum = 1
-      maximum = 10
-    }
-
-    rule {
-      metric_trigger {
-        metric_name        = "Percentage CPU"
-        metric_resource_id = azurerm_linux_virtual_machine_scale_set.cyral-sidecar-asg.id
-        time_grain         = "PT1M"
-        statistic          = "Average"
-        time_window        = "PT5M"
-        time_aggregation   = "Average"
-        operator           = "GreaterThan"
-        threshold          = 75
-        metric_namespace   = "microsoft.compute/virtualmachinescalesets"
-        dimensions {
-          name     = "AppName"
-          operator = "Equals"
-          values   = ["App1"]
-        }
-      }
-
-      scale_action {
-        direction = "Increase"
-        type      = "ChangeCount"
-        value     = "1"
-        cooldown  = "PT1M"
-      }
-    }
-
-    rule {
-      metric_trigger {
-        metric_name        = "Percentage CPU"
-        metric_resource_id = azurerm_linux_virtual_machine_scale_set.cyral-sidecar-asg.id
-        time_grain         = "PT1M"
-        statistic          = "Average"
-        time_window        = "PT5M"
-        time_aggregation   = "Average"
-        operator           = "LessThan"
-        threshold          = 25
-      }
-
-      scale_action {
-        direction = "Decrease"
-        type      = "ChangeCount"
-        value     = "1"
-        cooldown  = "PT1M"
-      }
+      default = var.auto_scale_default
+      minimum = var.auto_scale_min
+      maximum = var.auto_scale_max
     }
   }
 }
