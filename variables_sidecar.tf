bcdaab9fa60efcdacdb4b6771212109018e9dfd7
Cleber Gomes <cleberc@gmail.com>
2022-12-08 17:17:38 -0300

    ENG-10087: Remove DNS-related parameters in variables_sidecar.
    We should not have these in the AWS EC2 sidecar and should remove them from here.

--- variables_sidecar.tf
+++ variables_sidecar.tf
@@ -107,37 +107,6 @@ variable "sidecar_id" {
 #   default     = ""
 # }
 
-# ##########################################################################################################
-# # Sidecar endpoint possibilities:
-# #
-# # 1. In order to automatically create a DNS CNAME in Route53 to the sidecar, assign values to
-# #    `sidecar_dns_hosted_zone_id` and `sidecar_dns_name`. To update an existing DNS, it is also required
-# #    to assign `true` to `sidecar_dns_overwrite`. This DNS name will be shown in the UI instead of the
-# #    load balancer DNS;
-# # 2. In order to associate a DNS CNAME that will be managed manually (case of Snowflake sidecar), use the
-# #    variable `sidecar_dns_name` and leave `sidecar_dns_hosted_zone_id` and `sidecar_dns_overwrite` with
-# #    default values. In this case,the informed DNS name will be shown in the UI instead of the load balancer
-# #    DNS.
-# #
-# variable "sidecar_dns_hosted_zone_id" {
-#   description = "(Optional) Route53 hosted zone ID for the corresponding 'sidecar_dns_name' provided"
-#   type        = string
-#   default     = ""
-# }
-
-# variable "sidecar_dns_name" {
-#   description = "(Optional) Fully qualified domain name that will be automatically created/updated to reference the sidecar LB"
-#   type        = string
-#   default     = ""
-# }
-
-# variable "sidecar_dns_overwrite" {
-#   description = "(Optional) Update an existing DNS name informed in 'sidecar_dns_name' variable"
-#   type        = bool
-#   default     = false
-# }
-# ##########################################################################################################
-
 variable "sidecar_ports" {
   description = "List of ports allowed to connect to the sidecar. See also 'load_balancer_tls_ports'."
   type        = list(number)
